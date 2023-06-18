d67c99b7b9bfa222a2883898f5f8177adb57dc7b
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-18 21:48:18 +0100

    update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import useGameSocket from "@/hooks/socket";
 import { useRouter } from "next/router";
+import { Layout } from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,30 +10,32 @@ export default function Home() {
   const { rooms, createRoom, joinRoom } = useGameSocket({ router });
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <ul>
-        {rooms.map((room) => (
-          <li
-          className="cursor-pointer"
-            key={room.id}
-            onClick={() => {
-              joinRoom(room.id);
-            }}
-          >
-            {room.options.name}
-          </li>
-        ))}
-      </ul>
-
-      <button
-        onClick={() => {
-          createRoom({ name: "Hello World!" });
-        }}
+    <Layout>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
-        Create Room
-      </button>
-    </main>
+        <ul>
+          {rooms.map((room) => (
+            <li
+              className="cursor-pointer"
+              key={room.id}
+              onClick={() => {
+                joinRoom(room.id);
+              }}
+            >
+              {room.options.name}
+            </li>
+          ))}
+        </ul>
+
+        <button
+          onClick={() => {
+            createRoom({ name: "Hello World!" });
+          }}
+        >
+          Create Room
+        </button>
+      </main>
+    </Layout>
   );
 }
