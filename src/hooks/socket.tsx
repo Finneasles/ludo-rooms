c0cbef7502148c0c010d701e54e8525510ade01d
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-18 19:33:37 +0100

    Update socket.tsx

--- src/hooks/socket.tsx
+++ src/hooks/socket.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GameRoom } from "types";
 import { Connection } from "@/context/connection";
 import { NextRouter } from "next/router";
@@ -8,33 +8,50 @@ export const useGameSocket = ({
 }: { router?: NextRouter } | undefined) => {
   const { socket } = useContext(Connection);
   const [rooms, setRooms] = useState([]);
-  const [curRoom, setCuRoom] = useState({});
+  const [curRoom, setCurRoom] = useState({});
   const [curMsg, setCurMsg] = useState("");
   const [messages, setMessages] = useState([]);
 
-  if (!socket) return;
+  useEffect(() => {
+    socket.on("intData", (e) => {
+      setCurRoom(e.room);
+      setRooms(e.rooms);
+    });
 
-  socket.on("gotoRoom", (e) => {
-    setCuRoom(e);
-    router.push(
-      {
-        pathname: `/${e.id}`,
-      },
-      undefined,
-      { shallow: true }
-    );
-  });
+    socket.on("gotoRoom", (e) => {
+      console.log("going to", e);
+      const roomId = e.id;
+      router.push(
+        {
+          pathname: `/${roomId !== 0 ? roomId : ""}`,
+        },
+        undefined,
+        { shallow: true }
+      );
+    });
 
-  socket.on("setRooms", (e) => {
-    setRooms(e);
-  });
+    socket.on("setRooms", (e) => {
+      console.log("Rooms set", e);
+      setRooms(e);
+    });
 
-  socket.on("receiveMessage", (e) => {
-    setMessages([...messages, e]);
-  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    socket.on("receiveMessage", (e) => {
+      setMessages([...messages, e]);
+      console.log("Message added", messages);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages]);
 
   const createRoom = async (req: GameRoom) => {
     socket.emit("createRoom", req);
+  };
+
+  const leaveRoom = async () => {
+    socket.emit("leaveRoom");
   };
 
   const joinRoom = (req: GameRoom) => {
@@ -48,6 +65,7 @@ export const useGameSocket = ({
   return {
     sendMessage,
     createRoom,
+    leaveRoom,
     setCurMsg,
     joinRoom,
     messages,
