a5171b8034ce0e4cfe93cd816e30460b3e70f88c
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-07-03 06:19:32 +0100

    add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
+import { GAnalytics } from '@/components/ga'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GAnalytics/>
       <body>
         <Main />
         <NextScript />
