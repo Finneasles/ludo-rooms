7f3788f09c12810fe8256a58049f5d28ca9b3c87
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-07-03 04:12:10 +0100

    :zap: update pages

--- src/pages/[roomId].tsx
+++ src/pages/[roomId].tsx
@@ -1,60 +1,16 @@
-import { Inter } from "next/font/google";
 import { useGameSocket } from "@/hooks/socket";
 import { useRouter } from "next/router";
-import { ChatBox } from "@/components/chatbox";
-import { GameBoard } from "@/components/gameboard";
 import { Layout } from "@/components/layout";
+import { Room } from "@/components/pages/room";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+export default function RoomPage() {
   const router = useRouter();
-  const {
-    leaveRoom,
-    curRoom,
-    socket,
-    messages,
-    curMsg,
-    setCurMsg,
-    readyUp,
-    sendMessage,
-  } = useGameSocket(
-    router,
-  );
+  const { leaveRoom, socket, readyUp, connected, curRoom, isLoading } =
+    useGameSocket(router);
 
-  const handleInputChange = (event) => {
-    setCurMsg(event.target.value);
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      sendMessage(curMsg);
-      setCurMsg("");
-    }
-  };
   return (
-    <Layout>
-        <div className="w-full flex justify-center items-center h-full bg-gray-200">
-          {JSON.stringify(curRoom)}
-          <button
-            onClick={() => {
-              leaveRoom();
-            }}
-          >
-            Leave Room
-          </button>
-          <button
-            onClick={() => {
-              readyUp();
-            }}
-          >
-            Ready
-          </button>
-          <GameBoard />
-        </div>
-        <ChatBox
-          {...{ socket, messages, curMsg, handleKeyDown, handleInputChange }}
-        />
+    <Layout {...{ router, connected, isLoading }}>
+      <Room {...{ router, curRoom, leaveRoom, readyUp, socket }} />
     </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,38 +2,27 @@ import { Inter } from "next/font/google";
 import useGameSocket from "@/hooks/socket";
 import { useRouter } from "next/router";
 import { Layout } from "@/components/layout";
+import { Lobby } from "@/components/pages/lobby";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
-  const { rooms, createRoom, joinRoom } = useGameSocket(router);
+  const { rooms , createRoom, joinRoom, connected, isLoading } = useGameSocket(router);
 
   return (
-    <Layout>
-      <div className="h-[300px] w-96 right-0 overflow-y-auto top-0 bg-gray-300">
-        <div className="flex flex-col flex-grow overflow-auto space-y-2">
-        {rooms.map((room) => (
-          <div
-            className="cursor-pointer bg-red-200 w-full h-12"
-            key={room.id}
-            onClick={() => {
-              joinRoom(room.id);
-            }}
-          >
-            {room.options.name}
-          </div>
-        ))}
-
-        </div>
-      </div>
-      <button
-        onClick={() => {
-          createRoom({ name: "Hello World!" });
-        }}
-      >
-        Create Room
-      </button>
+    <Layout {...{ isLoading, router, connected }}>
+      <Lobby {...{ rooms, createRoom, joinRoom }} />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from an API or any other data source
+  const data = { /* your data */ };
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
